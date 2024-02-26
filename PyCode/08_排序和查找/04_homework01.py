f928ef54bbf58d7ebf5cc9fbeaf69fb408c3e178
Saber <1729357014@qq.com>
2024-02-26 23:29:06 +0800

    commit

--- PyCode/08_排序和查找/04_homework01.py
+++ PyCode/08_排序和查找/04_homework01.py
@@ -6,5 +6,18 @@
 @Author  :   Saber 
 ------------------------------------
 """
-
+import random
 # 随机生成10个整数(1-100的范围)保存到列表,使用冒泡排序,对其进行从大到小排序
+num_list = []
+for i in range(10):
+    num_list.append(random.randint(1, 101))
+print("排序前:", num_list)
+
+def bubble_sort(list):
+    for i in range(len(list) - 1):
+        for j in range(len(list) - 1 - i):
+            if list[j] < list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
+    return list
+bubble_sort(num_list)
+print("排序后:", num_list)
--- PyCode/08_排序和查找/05_homework02.py
+++ PyCode/08_排序和查找/05_homework02.py
@@ -6,4 +6,42 @@
 @Author  :   Saber 
 ------------------------------------
 """
+import random
+# 在homework01的基础上,使用二分查找,查找是否有8这个数,如果有,则返回对应的下标,如果没有,返回-1
+num_list = []
+for i in range(10):
+    num_list.append(random.randint(1, 101))
+print("排序前:", num_list)
 
+def bubble_sort(list):
+    for i in range(len(list) - 1):
+        for j in range(len(list) - 1 - i):
+            if list[j] < list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
+    return list
+bubble_sort(num_list)
+print("排序后:", num_list)
+
+def binary_search(list, find_val):
+    left = 0
+    right = len(list) - 1
+    find_index = -1
+    while left <= right:
+        mid = (left + right) //  2
+        if list[mid] > find_val:
+            left = mid + 1
+        elif list[mid] < find_val:
+            right = mid - 1
+        else:
+            find_index = mid
+            break
+    return find_index
+res_index = binary_search(num_list, 89)
+
+new_list = []
+if res_index == -1:
+    print("没找到:(")
+else:
+    new_list.append(res_index)
+    for ele in new_list:
+        print(f"找到了:),其下标为:{ele}")
