0596bea4e601cf06654661de460315319b2f088a
Saber <1729357014@qq.com>
2024-02-29 23:05:42 +0800

    commit\

--- PyCode/08_排序和查找/04_homework01.py
+++ PyCode/08_排序和查找/04_homework01.py
@@ -11,7 +11,7 @@ import random
 # 随机生成10个整数(1-100的范围)保存到列表,使用冒泡排序,对其进行从大到小排序
 num_list = []
 for i in range(10):
-    num_list.append(random.randint(1, 101))
+    num_list.append(random.randint(1, 100))
 print("排序前:", num_list)
 
 def bubble_sort(list):
