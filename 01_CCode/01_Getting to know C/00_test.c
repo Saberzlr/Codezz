67ac8456a292c2efcabb957070281b649ad8975d
Saber <1729357014@qq.com>
2024-03-30 18:51:49 +0800

    commit

--- 01_CCode/01_Getting to know C/00_test.c
+++ 01_CCode/01_Getting to know C/00_test.c
@@ -9,15 +9,9 @@
  */
 
 #include <stdio.h>
-// 指针变量的大小取决于地址的大小
-// 32位平台下地址是32个bit位（即4个字节）
-// 64位平台下地址是64个bit位（即8个字节）
+
 int main()
 {
-    printf("%d\n", sizeof(char *));   // 8
-    printf("%d\n", sizeof(short *));  // 8
-    printf("%d\n", sizeof(int *));    // 8
-    printf("%d\n", sizeof(double *)); // 8
 
     return 0;
 }
