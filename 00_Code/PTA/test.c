265d43c649ad8044c2670c687e7d64584c7f89fc
Saber <1729357014@qq.com>
2024-04-15 13:22:22 +0800

    commit

--- 00_Code/PTA/test.c
+++ 00_Code/PTA/test.c
@@ -11,38 +11,14 @@
 #include <stdio.h>
 int main()
 {
-    // 输入
-    int n, res;
-    scanf("%d", &n);
-    int arr[32] = {0};
-
-    // 十进制 -> 二进制
-    if (n >= 0)
+    for (int i = 5; i < 9; i++)
     {
-        for (int i = 0; n != 0; i++)
+        for (int j = 4; j > 9 - i; j++)
         {
-            arr[i] = n % 2;
-            n /= 2;
+            printf("*");
         }
-
-        for (int i = 31; i >= 0; i--)
-        {
-            if (arr[i] == 1)
-            {
-                res = 31 - i;
-                break;
-            }
-        }
-        // 32个数全为0 => 即n = 0的情况，前导32个0
-        res = 32;
+        printf("\n");
     }
-    else
-    {
-        res = 0;
-    }
-
-    // 输出
-    printf("%d", res);
 
     return 0;
 }
